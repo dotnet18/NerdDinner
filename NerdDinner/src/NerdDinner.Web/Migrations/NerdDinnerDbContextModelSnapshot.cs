8d9c1df7bd5df20581e4943c661d1fbe0749abbf
Gabriel Enea <dotnet18@hotmail.com>
2015-11-06 17:18:22 +0000

    Renamed web project folder to NerdDinner.Web and fixed gulp tasks for angular app

---
+++ NerdDinner/src/NerdDinner.Web/Migrations/NerdDinnerDbContextModelSnapshot.cs
@@ -0,0 +1,236 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using NerdDinner.Web.Persistence;
+
+namespace NerdDinner.Migrations
+{
+    [DbContext(typeof(NerdDinnerDbContext))]
+    partial class NerdDinnerDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Annotation("ProductVersion", "7.0.0-beta8-15964")
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityRole", b =>
+                {
+                    b.Property<string>("Id");
+
+                    b.Property<string>("ConcurrencyStamp")
+                        .IsConcurrencyToken();
+
+                    b.Property<string>("Name")
+                        .Annotation("MaxLength", 256);
+
+                    b.Property<string>("NormalizedName")
+                        .Annotation("MaxLength", 256);
+
+                    b.HasKey("Id");
+
+                    b.Index("NormalizedName")
+                        .Annotation("Relational:Name", "RoleNameIndex");
+
+                    b.Annotation("Relational:TableName", "AspNetRoles");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityRoleClaim<string>", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ClaimType");
+
+                    b.Property<string>("ClaimValue");
+
+                    b.Property<string>("RoleId");
+
+                    b.HasKey("Id");
+
+                    b.Annotation("Relational:TableName", "AspNetRoleClaims");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityUserClaim<string>", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ClaimType");
+
+                    b.Property<string>("ClaimValue");
+
+                    b.Property<string>("UserId");
+
+                    b.HasKey("Id");
+
+                    b.Annotation("Relational:TableName", "AspNetUserClaims");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityUserLogin<string>", b =>
+                {
+                    b.Property<string>("LoginProvider");
+
+                    b.Property<string>("ProviderKey");
+
+                    b.Property<string>("ProviderDisplayName");
+
+                    b.Property<string>("UserId");
+
+                    b.HasKey("LoginProvider", "ProviderKey");
+
+                    b.Annotation("Relational:TableName", "AspNetUserLogins");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole<string>", b =>
+                {
+                    b.Property<string>("UserId");
+
+                    b.Property<string>("RoleId");
+
+                    b.HasKey("UserId", "RoleId");
+
+                    b.Annotation("Relational:TableName", "AspNetUserRoles");
+                });
+
+            modelBuilder.Entity("NerdDinner.Web.Models.ApplicationUser", b =>
+                {
+                    b.Property<string>("Id");
+
+                    b.Property<int>("AccessFailedCount");
+
+                    b.Property<string>("ConcurrencyStamp")
+                        .IsConcurrencyToken();
+
+                    b.Property<string>("Email")
+                        .Annotation("MaxLength", 256);
+
+                    b.Property<bool>("EmailConfirmed");
+
+                    b.Property<bool>("LockoutEnabled");
+
+                    b.Property<DateTimeOffset?>("LockoutEnd");
+
+                    b.Property<string>("NormalizedEmail")
+                        .Annotation("MaxLength", 256);
+
+                    b.Property<string>("NormalizedUserName")
+                        .Annotation("MaxLength", 256);
+
+                    b.Property<string>("PasswordHash");
+
+                    b.Property<string>("PhoneNumber");
+
+                    b.Property<bool>("PhoneNumberConfirmed");
+
+                    b.Property<string>("SecurityStamp");
+
+                    b.Property<bool>("TwoFactorEnabled");
+
+                    b.Property<string>("UserName")
+                        .Annotation("MaxLength", 256);
+
+                    b.HasKey("Id");
+
+                    b.Index("NormalizedEmail")
+                        .Annotation("Relational:Name", "EmailIndex");
+
+                    b.Index("NormalizedUserName")
+                        .Annotation("Relational:Name", "UserNameIndex");
+
+                    b.Annotation("Relational:TableName", "AspNetUsers");
+                });
+
+            modelBuilder.Entity("NerdDinner.Web.Models.Dinner", b =>
+                {
+                    b.Property<long>("DinnerId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address")
+                        .IsRequired()
+                        .Annotation("MaxLength", 256);
+
+                    b.Property<string>("ContactPhone")
+                        .IsRequired()
+                        .Annotation("MaxLength", 32);
+
+                    b.Property<string>("Country")
+                        .Annotation("MaxLength", 64);
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .Annotation("MaxLength", 1024);
+
+                    b.Property<DateTime>("EventDate");
+
+                    b.Property<double>("Latitude");
+
+                    b.Property<double>("Longitude");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .Annotation("MaxLength", 64);
+
+                    b.Property<string>("UserName")
+                        .Annotation("MaxLength", 64);
+
+                    b.HasKey("DinnerId");
+                });
+
+            modelBuilder.Entity("NerdDinner.Web.Models.Rsvp", b =>
+                {
+                    b.Property<long>("RsvpId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<long>("DinnerId");
+
+                    b.Property<string>("UserName")
+                        .IsRequired()
+                        .Annotation("MaxLength", 64);
+
+                    b.HasKey("RsvpId");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityRoleClaim<string>", b =>
+                {
+                    b.HasOne("Microsoft.AspNet.Identity.EntityFramework.IdentityRole")
+                        .WithMany()
+                        .ForeignKey("RoleId");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityUserClaim<string>", b =>
+                {
+                    b.HasOne("NerdDinner.Web.Models.ApplicationUser")
+                        .WithMany()
+                        .ForeignKey("UserId");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityUserLogin<string>", b =>
+                {
+                    b.HasOne("NerdDinner.Web.Models.ApplicationUser")
+                        .WithMany()
+                        .ForeignKey("UserId");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole<string>", b =>
+                {
+                    b.HasOne("Microsoft.AspNet.Identity.EntityFramework.IdentityRole")
+                        .WithMany()
+                        .ForeignKey("RoleId");
+
+                    b.HasOne("NerdDinner.Web.Models.ApplicationUser")
+                        .WithMany()
+                        .ForeignKey("UserId");
+                });
+
+            modelBuilder.Entity("NerdDinner.Web.Models.Rsvp", b =>
+                {
+                    b.HasOne("NerdDinner.Web.Models.Dinner")
+                        .WithMany()
+                        .ForeignKey("DinnerId");
+                });
+        }
+    }
+}
---
+++ NerdDinner/src/NerdDinner.Web/gulpfile.js
@@ -0,0 +1,78 @@
+﻿/// <binding Clean='clean' />
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    project = require("./project.json"),
+    less = require('gulp-less');
+
+var paths = {
+    webroot: "./" + project.webroot + "/"
+};
+
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.views = paths.webroot + "views/";
+paths.images = paths.webroot + "images/";
+paths.concatNgAppJsDest = paths.webroot + "/app.js";
+paths.styles = paths.webroot + "css/",
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean:ng-html", function (cb) {
+    gulp.src('ng-apps/views/*.html')
+        .pipe(gulp.dest(paths.views));
+});
+
+gulp.task("clean:ng-images", function (cb) {
+    gulp.src('ng-apps/content/images/*.*')
+        .pipe(gulp.dest(paths.images));
+});
+
+gulp.task('less', function () {
+    return gulp.src('ng-apps/content/styles/*.less')
+      .pipe(less())
+      .pipe(gulp.dest(paths.styles));
+});
+
+gulp.task("clean", ["clean:js", "clean:css", "clean:ng-html", "clean:ng-images"]);
+
+gulp.task("min:js", function () {
+    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task('min:ng-apps-js', function () {
+    return gulp.src('ng-apps/**/*.js')
+      .pipe(concat(paths.concatNgAppJsDest))
+      .pipe(uglify({
+                mangle: false,
+                output: {
+                    beautify: true
+                }
+            }))
+      .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css", "min:ng-apps-js"]);
